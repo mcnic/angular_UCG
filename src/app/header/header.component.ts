95c9253f01a32699fe1dc6d0ab65440569b84d51
max <mcnic@users.noreply.github.com>
2023-09-10 19:27:38 -0400

    fix header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
+export type TMenus = 'recipe' | 'shopping-list';
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
 })
 export class HeaderComponent {
-
+  @Output() featureSelected = new EventEmitter<TMenus>();
+  onSelect(feature: TMenus) {
+    this.featureSelected.emit(feature);
+  }
 }
