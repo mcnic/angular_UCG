519e0ceb15374b9919619bbc8c9acf8d3a745ec7
max <mcnic@users.noreply.github.com>
2023-09-16 19:18:32 -0400

    move image to assets

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -13,12 +13,12 @@ export class RecipeListComponent implements OnInit {
     new Recipe(
       'A Test Recipe 1',
       'This is simply a test 1',
-      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      'assets/img/Recipe_logo.jpeg',
     ),
     new Recipe(
       'A Test Recipe 2',
       'This is simply a test 2',
-      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      'assets/img/Recipe_logo.jpeg',
     ),
   ];
 
